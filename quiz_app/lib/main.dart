ed9767df3cd6de911cd459e2cb614084144c1cf4
Mayank Roy <roy050703@gmail.com>
2022-02-23 21:04:26 +0530

    added styling and divided widget dart files

--- quiz_app/lib/main.dart
+++ quiz_app/lib/main.dart
@@ -1,22 +1,23 @@
-import 'dart:html';
-
 import 'package:flutter/material.dart';
+
+import './question.dart';
+import './answer.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
-    return MyAppState();
+    return _MyAppState();
   }
 }
 
-class MyAppState extends State<MyApp> {
-  var questionIndex = 0;
+class _MyAppState extends State<MyApp> {
+  var _questionIndex = 0;
 
-  void answerQuestion() {
+  void _answerQuestion() {
     setState(() {
-      questionIndex = questionIndex + 1;
+      _questionIndex = _questionIndex + 1;
     });
   }
 
@@ -33,21 +34,12 @@ class MyAppState extends State<MyApp> {
         ),
         body: Column(
           children: [
-            Text(
-              questions[questionIndex],
+            Question(
+              questions[_questionIndex],
             ),
-            RaisedButton(
-              child: Text('Answer 1'),
-              onPressed: answerQuestion,
-            ),
-            RaisedButton(
-              child: Text('Answer 2'),
-              onPressed: () => print('Answer 2 chosen!'),
-            ),
-            RaisedButton(
-              child: Text('Answer 3'),
-              onPressed: () => print('Answer 3 chosen!'),
-            ),
+            Answer(),
+            Answer(),
+            Answer(),
           ],
         ),
       ),
