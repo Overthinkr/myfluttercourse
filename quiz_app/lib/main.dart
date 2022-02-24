8f2518602fb2a4b6536c92c10f8502668a80454a
Mayank Roy <roy050703@gmail.com>
2022-02-24 14:23:51 +0530

    seperated quiz and answer widgets

--- quiz_app/lib/main.dart
+++ quiz_app/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-import './question.dart';
-import './answer.dart';
+import './quiz.dart';
+import './result.dart';
 
 void main() => runApp(MyApp());
 
@@ -13,47 +13,46 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  static const _questions = [
+    {
+      'questionText': 'What\'s your favorite color?',
+      'answers': ['blue', 'green', 'yellow', 'pink'],
+    },
+    {
+      'questionText': 'What\'s your favorite animal?',
+      'answers': ['bunny', 'koala', 'dog', 'giraffe'],
+    },
+    {
+      'questionText': 'Who\'s your favorite comedian?',
+      'answers': ['ME', 'ME', 'ME', 'ME'],
+    },
+  ];
+
   var _questionIndex = 0;
 
   void _answerQuestion() {
     setState(() {
       _questionIndex = _questionIndex + 1;
     });
+    if (_questionIndex < _questions.length) {
+      print('Aur questions hai bro');
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    var questions = [
-      {
-        'questionText': 'What\'s your favorite color?',
-        'answers': ['blue', 'green', 'yellow', 'pink'],
-      },
-      {
-        'questionText': 'What\'s your favorite animal?',
-        'answers': ['bunny', 'koala', 'dog', 'giraffe'],
-      },
-      {
-        'questionText': 'Who\'s your favorite comedian?',
-        'answers': ['ME', 'ME', 'ME', 'ME'],
-      },
-    ];
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('My First App'),
-        ),
-        body: Column(
-          children: [
-            Question(
-              questions[_questionIndex]['questionText'] as String,
-            ),
-            ...(questions[_questionIndex]['answers'] as List<String>)
-                .map((answer) {
-              return Answer(_answerQuestion, answer);
-            }).toList()
-          ],
-        ),
-      ),
+          appBar: AppBar(
+            title: Text('My First App'),
+          ),
+          body: _questionIndex < _questions.length
+              ? Quiz(
+                  answerQuestion: _answerQuestion,
+                  questionIndex: _questionIndex,
+                  questions: _questions,
+                )
+              : Result()),
     );
   }
 }
